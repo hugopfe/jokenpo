bc3bea406f60108958f8489f02df748ff7a40156
hugopfe <hgp.fernandes04@gmail.com>
2026-07-28 19:47:09 -0300

    Created Game class with methods to better organization

--- script.js
+++ script.js
@@ -1,94 +1,112 @@
-const Valid_Plays = Object.freeze({
-  ROCK: "Rock",
-  PAPER: "Paper",
-  SCISSORS: "Scissors",
-})
+/*
+“o que é regra do jogo?”
+“o que é estado?”
+“o que é interface?”
+*/
 
-const Play_Definitions = Object.freeze({
-  [Valid_Plays.ROCK]: {
-    id: Valid_Plays.ROCK,
-    wins: Valid_Plays.SCISSORS,
-    loses: Valid_Plays.PAPER,
-    icon: "&#9994;",
-  },
-  [Valid_Plays.PAPER]: {
-    id: Valid_Plays.PAPER,
-    wins: Valid_Plays.ROCK,
-    loses: Valid_Plays.SCISSORS,
-    icon: "&#9995;",
-  },
-  [Valid_Plays.SCISSORS]: {
-    id: Valid_Plays.SCISSORS,
-    wins: Valid_Plays.PAPER,
-    loses: Valid_Plays.ROCK,
-    icon: "&#9996;",
-  },
-})
+class Game {
+  static Play_Order = Object.freeze(["ROCK", "PAPER", "SCISSORS"])
 
-const game = {
-  validPlays: [
-    Play_Definitions[Valid_Plays.ROCK],
-    Play_Definitions[Valid_Plays.PAPER],
-    Play_Definitions[Valid_Plays.SCISSORS],
-  ],
-  playerScore: 0,
-  pcScore: 0,
-  currentRound: 0,
+  static Play_Definitions = Object.freeze({
+    ROCK: {
+      id: "ROCK",
+      wins: "SCISSORS",
+      loses: "PAPER",
+      icon: "&#9994;",
+    },
+    PAPER: {
+      id: "PAPER",
+      wins: "ROCK",
+      loses: "SCISSORS",
+      icon: "&#9995;",
+    },
+    SCISSORS: {
+      id: "SCISSORS",
+      wins: "PAPER",
+      loses: "ROCK",
+      icon: "&#9996;",
+    },
+  })
+
+  constructor() {
+    this.state = {
+      playerScore: 0,
+      pcScore: 0,
+      currentRound: 0,
+    }
+  }
+
+  /**
+   * Handles players choices (user and PC)
+   */
+  play(userChoice) {
+    const userPlay = Game.Play_Definitions[userChoice]
+    const pcPlay = this.getRandomPlay()
+    const result = this.resolveRound(userPlay, pcPlay)
+
+    this.render(userPlay, pcPlay, result)
+  }
+
+  /*
+   * Returns a random valid play
+   */
+  getRandomPlay() {
+    const randomIndex = Math.floor(Math.random() * Game.Play_Order.length)
+    console.log(Game.Play_Definitions[Game.Play_Order[randomIndex]])
+    
+    return Game.Play_Definitions[Game.Play_Order[randomIndex]]
+  }
+
+  /**
+   * Verify who is the winner
+   */
+  resolveRound(userPlay, pcPlay) {
+    this.state.currentRound++
+
+    if (userPlay.wins === pcPlay.id) {
+      this.state.playerScore++
+      return "Player wins"
+    } else if (userPlay.loses === pcPlay.id) {
+      this.state.pcScore++
+      return "Player loses"
+    }
+
+    return "Draw"
+  }
+
+  /**
+   * Update the interface with game state
+   */
+  render(userPlay, pcPlay, result) {
+    document.querySelector("#round span").textContent = this.state.currentRound
+    document.querySelector("#player-score span").textContent =
+      this.state.playerScore
+    document.querySelector("#pc-score span").textContent = this.state.pcScore
+
+    const userPlayDiv = document.querySelector("#user-side .play")
+    userPlayDiv.classList.remove("awaiting")
+
+    const pcPlayDiv = document.querySelector("#pc-side .play")
+    pcPlayDiv.classList.remove("awaiting")
+
+    userPlayDiv.innerHTML = userPlay.icon
+    pcPlayDiv.innerHTML = pcPlay.icon
+
+    const matchResult = document.querySelector("#match-result")
+    matchResult.textContent = result
+    matchResult.style.animationName = "shake"
+  }
+
+  /**
+   * Resets the match
+   */
+  reset() {}
 }
 
-const roundParagraph = document.querySelector("#round span")
-const playerScoreParagraph = document.querySelector("#player-score span")
-const pcScoreParagraph = document.querySelector("#pc-score span")
 const matchResult = document.querySelector("#match-result")
 
-matchResult.addEventListener("animationend", (ev) => {
+matchResult.addEventListener("animationend", () => {
   matchResult.style.animationName = ""
 })
 
-const increasePlayerScore = () => game.playerScore++
-const increasePcScore = () => game.pcScore++
-
-function play(userPlayIndex) {
-  const userPlayDiv = document.querySelector("#user-side .play")
-  userPlayDiv.classList.remove("awaiting")
-  const pcPlayDiv = document.querySelector("#pc-side .play")
-  pcPlayDiv.classList.remove("awaiting")
-  const pcPlayIndex = Math.floor(Math.random() * game.validPlays.length)
-
-  userPlay = game.validPlays[userPlayIndex]
-  pcPlay = game.validPlays[pcPlayIndex]
-
-  userPlayDiv.innerHTML = userPlay.icon
-  pcPlayDiv.innerHTML = pcPlay.icon
-
-  checkWinner(userPlay, pcPlay)
-  updateGame()
-}
-
-function checkWinner(userPlay, pcPlay) {
-  let result
-
-  if (userPlay.wins == pcPlay.id) {
-    increasePlayerScore()
-    result = "Player wins"
-  } else if (userPlay.loses == pcPlay.id) {
-    increasePcScore()
-    result = "Player loses"
-  } else {
-    result = "Draw"
-  }
-
-  displayResult(result)
-}
-
-function updateGame() {
-  game.currentRound++
-  roundParagraph.innerHTML = game.currentRound
-  playerScoreParagraph.innerHTML = game.playerScore
-  pcScoreParagraph.innerHTML = game.pcScore
-}
-
-function displayResult(result) {
-  matchResult.innerHTML = result
-  matchResult.style.animationName = "shake"
-}
+const game = new Game()
